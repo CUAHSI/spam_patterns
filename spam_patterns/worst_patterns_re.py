2e5442cba57c1da8395755097cb63e73e8f3ba2a
Devin Cowan <dcowan@cuahsi.org>
2023-10-02 15:12:43 -0400

    narrow patterns

--- spam_patterns/worst_patterns_re.py
+++ spam_patterns/worst_patterns_re.py
@@ -1,6 +1,4 @@
 patterns = [
-    "amazing",
-    "business",
     "cheap[est]?",
     "phone number",
     "4free",
@@ -8,31 +6,22 @@ patterns = [
     # r'' is a 'raw' string (backslash symbol is treated as a literal
     # backslash).
     r"\d-[\d]{3}-[\d]{3}-[\d]{4}",
-    "airline[s]?",
     "baggage",
     "booking",
-    "vacation[al]?",
-    "ticket[s]?",
     "antivirus",
     "cleaner",
     "cookies",
     "laptop",
-    "password",
     # r'' is a 'raw' string (backslash symbol is treated as a literal
     # backslash).
     # '\b' stands for 'word boundary'.
     "android",
-    r"\bchrome\b",
     r"\bapple\b",
     "icloud",
-    r"\bios\b",
     "iphone",
     "macbook",
     "macos",
-    "facebook",
-    "microsoft",
     "internet explorer",
     "escort",
     "porn",
-    "xxx",
 ]
